1060d76a173493c61a0c57bf2106ea9685ccc049
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2024-10-13 18:06:35 +0600

    Added Copy Constructor , Assignment Operator and One arg Constructor

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -5,36 +5,6 @@
 
 bool failed = false;
 
-void settingDimensionsTest();
-void settingComponentsWithConstructor();
-void settingComponentsWithSetter();
-void checking_dimension_mismatch_with_higher_value();
-void checking_dimension_mismatch_with_lower_value();
-
-
-int main (int argc, char *argv[]) {
-  std::cout << "Test 1 : Creating Vector\n";
-  
-  //test 1.1
-  settingDimensionsTest();
-
-   //test 1.2
-  settingComponentsWithConstructor();
-
-  //test 1.3
-  settingComponentsWithSetter();
-
-  //test 1.4
-  checking_dimension_mismatch_with_higher_value();
-
-  //test 1.5
-  checking_dimension_mismatch_with_lower_value();
-
-  if(!failed)
-      std::cout << "\n\n\nAll Test Passed\n";
-  return 0;
-}
-
 void settingDimensionsTest(){
   int n = 2;
   VectorND<int>v(n);
@@ -126,6 +96,112 @@ void checking_dimension_mismatch_with_lower_value(){
       std::cout << "\n\tTest 1.5 Passed\n";
   }
 
+void checking_copy_constructor(){
+  std::cout << "Test 1.6 : Checking Copy Constructor";
+  int n = 3;
+  VectorND<int>v(n);
 
+  std::vector<int>ar = {1 , 2 , 3};
+  v.setComponents(ar);
+
+  VectorND<int>v2(v);
+
+  if(v2.getDimension() != v.getDimension()) {
+    std::cout<< "Test 1.6 Failed : dimension of v : "<< v2.getDimension() <<" is not equal to diemnsion of v2 : " << v.getDimension() << " \n";
+    failed = true;
+    return;
+  }
+
+  for(int i=0; i<n; i++){
+    if(v2[i] != v[i]) {
+      std::cout << "Test 1.6 Failed : at index " << i << v2[i] << "!=" << v[i] << "\n";
+      failed = true;
+      return;
+    }
+  }
+  std::cout<< "\tTest 1.6 Passed\n";
+}
+
+void checking_assignment_operator(){
+
+  std::cout << "Test 1.7 : Checking Assignment Operator";
+  int n = 3;
+  VectorND<int>v(n);
+
+  std::vector<int>ar = {1 , 2 , 3};
+  v.setComponents(ar);
+
+  VectorND<int>v2 = v;
+
+  if(v2.getDimension() != v.getDimension()) {
+    std::cout<< "Test 1.7 Failed : dimension of v : "<< v2.getDimension() <<" is not equal to diemnsion of v2 : " << v.getDimension() << " \n";
+    failed = true;
+    return;
+  }
+
+  for(int i=0; i<n; i++){
+    if(v2[i] != v[i]) {
+      std::cout << "Test 1.7 Failed : at index " << i << v2[i] << "!=" << v[i] << "\n";
+      failed = true;
+      return;
+    }
+  }
+  std::cout<< "\tTest 1.7 Passed\n";
+}
+
+void testing_only_vector_constructor(){
+ std::cout << "Test 1.8 : Testing Vector Only Constructor";
+
+  std::vector<int>ar = {1 , 2 , 3};
+  VectorND<int>v(ar);
+  
+
+  if(ar.size() != v.getDimension()) {
+    std::cout<< "Test 1.8 Failed : dimension of v : "<< v.getDimension() <<" is not equal to diemnsion of array : " << ar.size() << " \n";
+    failed = true;
+    return;
+  }
+
+  for(int i=0; i<ar.size(); i++){
+    if(ar[i] != v[i]) {
+      std::cout << "Test 1.8 Failed : at index " << i << ar[i] << "!=" << v[i] << "\n";
+      failed = true;
+      return;
+    }
+  }
+  std::cout<< "\tTest 1.8 Passed\n";
+
+}
+
+int main (int argc, char *argv[]) {
+  std::cout << "Test 1 : Creating Vector\n";
+  
+  //test 1.1
+  settingDimensionsTest();
+
+   //test 1.2
+  settingComponentsWithConstructor();
+
+  //test 1.3
+  settingComponentsWithSetter();
+
+  //test 1.4
+  checking_dimension_mismatch_with_higher_value();
+
+  //test 1.5
+  checking_dimension_mismatch_with_lower_value();
+  
+  //test1.6
+  checking_copy_constructor();
+
+  //test1.7
+  checking_assignment_operator();
+  
+  //test1.8
+  testing_only_vector_constructor();
+  if(!failed)
+      std::cout << "\n\n\nAll Test Passed\n";
+  return 0;
+}
 
 
