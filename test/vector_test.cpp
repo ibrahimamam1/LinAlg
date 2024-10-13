4f309b85a45837d0de70e957ae46091abac47b9a
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2024-10-13 17:24:09 +0600

    Added guard for dimension mismatch

--- test/vector_test.cpp
+++ test/vector_test.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <vector>
 #include "../src/vector.h"
@@ -7,6 +8,9 @@ bool failed = false;
 void settingDimensionsTest();
 void settingComponentsWithConstructor();
 void settingComponentsWithSetter();
+void checking_dimension_mismatch_with_higher_value();
+void checking_dimension_mismatch_with_lower_value();
+
 
 int main (int argc, char *argv[]) {
   std::cout << "Test 1 : Creating Vector\n";
@@ -19,6 +23,12 @@ int main (int argc, char *argv[]) {
 
   //test 1.3
   settingComponentsWithSetter();
+
+  //test 1.4
+  checking_dimension_mismatch_with_higher_value();
+
+  //test 1.5
+  checking_dimension_mismatch_with_lower_value();
 
   if(!failed)
       std::cout << "\n\n\nAll Test Passed\n";
@@ -77,9 +87,44 @@ void settingComponentsWithSetter(){
   std::cout <<"\tTest 1.3 Passed\n"; 
 } 
 
+void checking_dimension_mismatch_with_higher_value(){
+  std::cout << "Test 1.4 : Testing Dimension Mistmatch With Higher Value\n";
 
+  int n = 4;
+  VectorND<int>v(n);
 
+  std::vector<int>ar(5);
+  ar = {1 , 2 , 3 , 4 , 5};
+  
+  try{
+    v.setComponents(ar);
+    std::cout << "Test 1.4 failed\n";
+    failed = true;
+  }catch(std::exception e){
+    std::cout << e.what();
+    std::cout << "\n\tTest 1.4 Passed\n";
+  }
+ }
 
+void checking_dimension_mismatch_with_lower_value(){
+  std::cout << "Test 1.5 : Testing Dimension Mistmatch With Lower Value\n";
+
+  int n = 7;
+  VectorND<int>v(n);
+
+  std::vector<int>ar(5);
+  ar = {1 , 2 , 3 , 4 , 5};
+  
+    v.setComponents(ar);
+    for(int i=n-1; i<ar.size(); i++){
+     if(v[i] != 0){
+      std::cout << "Test 1.5 failed\n At index %d " << i << v[n-1] << "!= 0\n";
+      failed = true;
+      return;
+      }
+  }
+      std::cout << "\n\tTest 1.5 Passed\n";
+  }
 
 
 
